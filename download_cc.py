b263500f542a9f165ce5c7dfdf574b6ce9543568
Ben Wang <wangben3@gmail.com>
2021-02-27 12:05:45 +1100

    shard output folder

--- download_cc.py
+++ download_cc.py
@@ -4,11 +4,15 @@ from functools import partial
 from tqdm import tqdm
 
 import sys
+import pathlib
 
 
 def process_wat(url, output_path):
     output_name = url.split("/")[-1].replace(".warc.wat.gz", ".jsonl.wat.gz")
-    subprocess.run(["./commoncrawl_filter", "http://commoncrawl.s3.amazonaws.com/" + url, f"{output_path}/{output_name}".strip()])
+    dir_name = url.split("/")[1]
+
+    pathlib.Path(f"{output_path}/{dir_name}/").mkdir(parents=True, exist_ok=True)
+    subprocess.run(["./commoncrawl_filter", "http://commoncrawl.s3.amazonaws.com/" + url, f"{output_path}/{dir_name}/{output_name}".strip()])
 
 
 assert len(sys.argv) == 4
